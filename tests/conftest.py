928aa09f05f585c73fc80377db04161e65aeaeb7
Schlagonia <schlagonia@gmail.com>
2023-03-26 09:34:38 -0600

    fix: black

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def tokens():
     tokens = {
         "weth": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
         "dai": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-        "usdc": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+        "usdc": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     }
     yield tokens
 
@@ -97,7 +97,7 @@ def create_strategy(management, keeper, asset):
 @pytest.fixture(scope="session")
 def strategy(asset, create_strategy):
     strategy = create_strategy(asset)
-    
+
     yield strategy
 
 
