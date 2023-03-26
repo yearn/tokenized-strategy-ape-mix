8535299eb86f2ea9855a8bcaa19ab777df65a01a
Schlagonia <schlagonia@gmail.com>
2023-03-26 09:56:41 -0600

    fix: interface

--- contracts/interfaces/IStrategy.sol
+++ contracts/interfaces/IStrategy.sol
@@ -1,30 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.18;
 
-import {IBaseLibrary} from "@tokenized-strategy/interfaces/IBaseLibrary.sol";
+import {IStrategy} from "@tokenized-strategy/interfaces/IStrategy.sol";
 
-interface IStrategy is IBaseLibrary {
-    /*//////////////////////////////////////////////////////////////
-                            IMMUTABLE FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    function isOriginal() external view returns (bool);
-
-    function initialize(
-        address _asset,
-        string memory name_,
-        address _management,
-        address _performanceFeeRecipient,
-        address _keeper
-    ) external;
-
-    function availableDepositLimit(
-        address _owner
-    ) external view returns (uint256);
-
-    function availableWithdrawLimit(
-        address _owner
-    ) external view returns (uint256);
-
-    function tendTrigger() external view returns (bool);
-}
